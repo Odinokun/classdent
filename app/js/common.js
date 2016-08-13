daaa989cd2e8a4c311652dbd16e04927bf26d62a
odinokun <odinokun@gmail.com>
2016-08-13 19:57:50 +0300

    complete  responsive index.html

--- app/js/common.js
+++ app/js/common.js
@@ -22,40 +22,6 @@ $(function() {
 })
 
 
-// ===== Выравнивание высоты фона у infoboard__item =====
-// === при загрузке ===
-$(function() {
-    jQuery(function($){
-        var max_col_height = 0;
-        $('.infoboard__item').each(function(){
-            if ($(this).height() > max_col_height) {
-                max_col_height = $(this).height();
-            }
-        });
-        $('.infoboard__item').height(max_col_height);
-    });
-});
-// === при ресайзе ===
-$(function() {
-    var columns = $('.infoboard__item');
-    $(window).resize(function () {
-        setEqualHeight(columns);
-    });
-
-    function setEqualHeight(columns) {
-        var tallestcolumn = 0;
-        columns.css('height', '');
-        columns.each(function() {
-            currentHeight = $(this).height();
-            if(currentHeight > tallestcolumn) {
-                tallestcolumn = currentHeight;
-            }
-        });
-        columns.height(tallestcolumn);
-    }
-});
-
-
 // ===== Выравнивание высоты фона у personal-sec__item =====
 // === при загрузке ===
 $(function() {
